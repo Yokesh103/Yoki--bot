b147c9d4ac772bc7c4fcbfd25e4255a4049f4c08
Yokesh <yokeshv33@gmail.com>
2025-11-27 22:54:29 +0530

    live service

--- Yoki-bot-minimal/Live-Feed-Microservice/app/main.py
+++ Yoki-bot-minimal/Live-Feed-Microservice/app/main.py
@@ -2,23 +2,23 @@ from fastapi import FastAPI
 from app.upstox_ws import start_ws
 from app.redis_client import redis_client
 import json
+from threading import Thread
+from urllib.parse import unquote
 
 app = FastAPI(title="Live Feed Microservice")
 
-
 @app.on_event("startup")
 def boot():
-    start_ws()
-
+    Thread(target=start_ws, daemon=True).start()
 
 @app.get("/health")
 def health():
     return {"status": "LIVE FEED RUNNING"}
 
-
 @app.get("/tick/{instrument_key}")
 def get_tick(instrument_key: str):
-    data = redis_client.get(instrument_key)
+    decoded_key = unquote(instrument_key)
+    data = redis_client.get(decoded_key)
 
     if not data:
         return {"error": "No live data yet"}
