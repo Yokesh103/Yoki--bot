eb0fbf5ace6501f8f3c2696dba81691d6abb1d9e
Yokesh <yokeshv33@gmail.com>
2025-11-27 23:28:00 +0530

    live service

--- Yoki-bot-minimal/Live-Feed-Microservice/app/upstox_ws.py
+++ Yoki-bot-minimal/Live-Feed-Microservice/app/upstox_ws.py
@@ -1,48 +1,53 @@
 import json
 import websocket
+import time
 from app.redis_client import redis_client
 from app.config import UPSTOX_ACCESS_TOKEN
 
 WS_URL = "wss://api.upstox.com/v2/feed/market-data-feed"
 
 
-def on_message(ws, message):
-    data = json.loads(message)
-    feeds = data.get("feeds", {})
+def start_ws(heartbeat_callback=None):
 
-    for instrument_key, content in feeds.items():
-        market = content.get("ff", {}).get("marketFF", {})
+    def on_message(ws, message):
+        if heartbeat_callback:
+            heartbeat_callback()
 
-        payload = {
-            "ltp": market.get("ltp"),
-            "oi": market.get("oi", 0)
-        }
+        data = json.loads(message)
+        feeds = data.get("feeds", {})
 
-        redis_client.set(instrument_key, json.dumps(payload))
-        print(f"📡 TICK RECEIVED -> {instrument_key} | LTP: {payload['ltp']} | OI: {payload['oi']}")
+        for instrument_key, content in feeds.items():
+            market = content.get("ff", {}).get("marketFF", {})
 
+            payload = {
+                "ltp": market.get("ltp"),
+                "oi": market.get("oi", 0),
+                "timestamp": int(time.time())
+            }
 
-def on_open(ws):
-    print("✅ Upstox WebSocket Connected & Subscribed")
+            redis_client.set(instrument_key, json.dumps(payload))
+            print(f"📡 {instrument_key} | LTP: {payload['ltp']} | OI: {payload['oi']}")
 
-    ws.send(json.dumps({
-        "guid": "live-feed",
-        "method": "sub",
-        "data": {
-            "mode": "full",
-            "instrumentKeys": [
-                "NSE_INDEX|Nifty 50",
-                "NSE_FO|46879"
-            ]
-        }
-    }))
+    def on_open(ws):
+        print("✅ Upstox WebSocket Connected")
 
+        ws.send(json.dumps({
+            "guid": "live-feed",
+            "method": "sub",
+            "data": {
+                "mode": "full",
+                "instrumentKeys": [
+                    "NSE_INDEX|Nifty 50",
+                    "NSE_FO|46879"
+                ]
+            }
+        }))
 
-def start_ws():
     ws = websocket.WebSocketApp(
         WS_URL,
         header=[f"Authorization: Bearer {UPSTOX_ACCESS_TOKEN}"],
         on_message=on_message,
         on_open=on_open
     )
+
     ws.run_forever()
