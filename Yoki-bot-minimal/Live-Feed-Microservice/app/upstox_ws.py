5e4a91c3f861a48bce46135b2ab40f768949c2c3
Yokesh <yokeshv33@gmail.com>
2025-11-27 22:58:35 +0530

    live service

--- Yoki-bot-minimal/Live-Feed-Microservice/app/upstox_ws.py
+++ Yoki-bot-minimal/Live-Feed-Microservice/app/upstox_ws.py
@@ -1,3 +1,11 @@
+import json
+import websocket
+from app.redis_client import redis_client
+from app.config import UPSTOX_ACCESS_TOKEN
+
+WS_URL = "wss://api.upstox.com/v2/feed/market-data-feed"
+
+
 def on_message(ws, message):
     data = json.loads(message)
     feeds = data.get("feeds", {})
@@ -12,3 +20,29 @@ def on_message(ws, message):
 
         redis_client.set(instrument_key, json.dumps(payload))
         print(f"📡 TICK RECEIVED -> {instrument_key} | LTP: {payload['ltp']} | OI: {payload['oi']}")
+
+
+def on_open(ws):
+    print("✅ Upstox WebSocket Connected & Subscribed")
+
+    ws.send(json.dumps({
+        "guid": "live-feed",
+        "method": "sub",
+        "data": {
+            "mode": "full",
+            "instrumentKeys": [
+                "NSE_INDEX|Nifty 50",
+                "NSE_FO|46879"
+            ]
+        }
+    }))
+
+
+def start_ws():
+    ws = websocket.WebSocketApp(
+        WS_URL,
+        header=[f"Authorization: Bearer {UPSTOX_ACCESS_TOKEN}"],
+        on_message=on_message,
+        on_open=on_open
+    )
+    ws.run_forever()
